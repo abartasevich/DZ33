f9eadd7a6d98ee1aa6e0641d774009f543bd6ab4
Andrei <abartasevich@vtb.ru>
2020-12-06 11:53:33 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double a = 3600;
+        //секунд в часе
+        double b = 2400;
+        //переменное значение, вводится для выполнения конвертации секунд в часы
+        double  c = a/b;
+        //значение конвертации
+
+        System.out.println(c);
+    
+    }
+}
